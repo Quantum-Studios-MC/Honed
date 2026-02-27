1c434b1f1edc68b41d0ae40c8bb31207e6d714d7
Siepert123 <createlegacy69@gmail.com>
2026-02-27 13:50:54 +0100

    various abstract objects

--- src/main/java/quantumstudios/honed/Honed.java
+++ src/main/java/quantumstudios/honed/Honed.java
@@ -4,6 +4,9 @@ import net.minecraft.block.Block;
 import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.network.FMLEventChannel;
+import net.minecraftforge.fml.common.network.NetworkRegistry;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 import quantumstudios.honed.Tags;
@@ -13,8 +16,10 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import net.minecraftforge.event.RegistryEvent;
+import quantumstudios.honed.network.HonedNetworkManager;
 import quantumstudios.honed.proxy.CommonProxy;
 import quantumstudios.honed.registry.HonedBlocks;
+import quantumstudios.honed.registry.HonedGUIs;
 import quantumstudios.honed.registry.HonedItems;
 import quantumstudios.honed.data.loader.JsonDataLoader;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -26,6 +31,12 @@ public class Honed {
 
     public static final Logger LOGGER = LogManager.getLogger(Tags.MOD_NAME);
 
+    @Mod.Instance
+    private static Honed instance;
+    public static Honed instance() {
+        return instance;
+    }
+
     @SidedProxy(
             serverSide = CommonProxy.SERVER_PROXY,
             clientSide = CommonProxy.CLIENT_PROXY
@@ -35,6 +46,8 @@ public class Honed {
         return sidedProxy;
     }
 
+    public static final FMLEventChannel NETWORK = NetworkRegistry.INSTANCE.newEventDrivenChannel(Tags.MOD_ID);
+
     /**
      * <a href="https://cleanroommc.com/wiki/forge-mod-development/event#overview">
      *     Take a look at how many FMLStateEvents you can listen to via the @Mod.EventHandler annotation here
@@ -43,13 +56,26 @@ public class Honed {
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event) {
         LOGGER.info("Hello From {}!", Tags.MOD_NAME);
+        NETWORK.register(new HonedNetworkManager());
         JsonDataLoader.setConfigPath(event.getModConfigurationDirectory().toPath().resolve("honed"));
         JsonDataLoader.load();
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {
+        NetworkRegistry.INSTANCE.registerGuiHandler(this, new HonedGUIs.Handler());
     }
 
     @SubscribeEvent
     public static void registerBlocks(RegistryEvent.Register<Block> event) {
         HonedBlocks.register(event);
+        HonedBlocks.registerTEs();
+    }
+
+    @SideOnly(Side.CLIENT)
+    @SubscribeEvent
+    public static void registerTESRs(RegistryEvent.Register<Block> event) {
+        HonedBlocks.registerTESRs();
     }
 
     @SubscribeEvent
